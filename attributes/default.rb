1cd869a73e0fe1a71b44d51e17119594af961606
Pirate Dunbar <yarr@piratedunbar.com>
2014-01-30 22:39:59 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
-
 default[:ffmpeg][:install_method] = "source"
 
 default[:ffmpeg][:version] = "2.1"
 default[:ffmpeg][:source_url] = "http://ffmpeg.org/releases/ffmpeg-#{default[:ffmpeg][:version]}.tar.gz"
 default[:ffmpeg][:source_prefix] = "/opt"
+default[:ffmpeg][:options] = "/opt"
+default[:ffmpeg][:autoconf_opts] = ['--enable-shared', '--enable-pthreads', '--enable-gpl', '--enable-version3', '--enable-nonfree', '--enable-hardcoded-tables', '--enable-avresample', '--enable-vda', '--cc=clang', '--enable-libx264', '--enable-libfaac', '--enable-libmp3lame', '--enable-libxvid', '--enable-libtheora', '--enable-libvorbis', '--enable-libvpx', '--enable-libass', '--enable-libspeex', '--enable-libschroedinger', '--enable-libopus', '--enable-libopenjpeg'];
