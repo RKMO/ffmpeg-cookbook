16522670f7e7f69b1cb51bd6bb93012e579d99ea
Ptah Dunbar <yarr@piratedunbar.com>
2014-02-03 06:40:51 -0500

    Install ffmpeg with custom config

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              "ffmpeg"
 maintainer        "Sergey Khaladzinski"
-version           "0.0.1"
+version           "0.0.9"
 
 depends "build-essential"
 depends "ark", "~> 0.3"
--- recipes/source.rb
+++ recipes/source.rb
@@ -1,11 +1,73 @@
 include_recipe "ark"
-
-template '/etc/apt/sources.list.d/libfaac-dev.list' do
-  notifies :run, 'execute[apt-get update]', :immediately
-end
+include_recipe "yasm"
 
 node[:ffmpeg][:packages].each do |pkg|
   package pkg
+end
+
+directory "#{node[:ffmpeg][:source_prefix]}/ffmpeg-#{node[:ffmpeg][:version]}" do
+    recursive true
+    action :delete
+end
+
+remote_file "/etc/apt/source.list.d/faac.list" do
+    source "libfaac-dev.list.erb"
+    notifies :run, resources(:execute => "apt-get update"), :immediately
+end
+
+package "faac"
+
+
+directory "/opt/ffmpeg_sources"
+
+bash "install yasm" do
+  cwd "/opt/ffmpeg_sources"
+  code <<-EOH
+      git clone git://github.com/yasm/yasm.git
+      cd yasm
+      sh autogen.sh
+      make
+      sudo checkinstall --pkgname=yasm --pkgversion="1.1.0" --backup=no --deldoc=yes --default
+  EOH
+end
+
+bash "install lame" do
+  user "root"
+  cwd "/opt/ffmpeg_sources"
+  code <<-EOH
+        wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.tar.gz
+        tar xzvf lame-3.99.tar.gz
+        cd lame-3.99
+        ./configure
+        make
+        sudo checkinstall --pkgname=lame-ffmpeg --pkgversion="3.99" --backup=no --default --deldoc=yes
+  EOH
+end
+
+bash "install opencore-amr" do
+  user "root"
+  cwd "/opt/ffmpeg_sources"
+  code <<-EOH
+        wget http://downloads.sourceforge.net/project/opencore-amr/vo-amrwbenc/vo-amrwbenc-0.1.1.tar.gz
+        tar zxvf vo-amrwbenc-0.1.1.tar.gz
+        cd vo-amrwbenc-0.1.1
+        ./configure --disable-shared
+        make
+        sudo checkinstall --pkgname="libopencore-amr" --pkgversion="0.1.1" --backup=no --fstrans=no --install=yes --default
+  EOH
+end
+
+bash "install libtheora" do
+  user "root"
+  cwd "/opt/ffmpeg_sources"
+  code <<-EOH
+        wget http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2
+        tar jxvf libtheora-1.1.1.tar.bz2
+        cd libtheora-1.1.1
+        ./configure --disable-shared
+        make
+        sudo checkinstall --pkgname=libtheora --pkgversion "1.1.1" --backup=no --fstrans=no --install=yes --default
+  EOH
 end
 
 ark "ffmpeg" do
@@ -13,5 +75,5 @@ ark "ffmpeg" do
   prefix_root node[:ffmpeg][:source_prefix]
   version node[:ffmpeg][:version]
   action [ :configure, :install_with_make ]
-  autoconf_opts node[:ffmpeg][:autoconf_opts] if node[:ffmpeg][:autoconf_opts]
+  #autoconf_opts node[:ffmpeg][:autoconf_opts] if node[:ffmpeg][:autoconf_opts]
 end
