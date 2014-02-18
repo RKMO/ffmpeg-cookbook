4af39d9deed0a52ab48d2e189516c57fa0619d42
Pirate Dunbar <yarr@piratedunbar.com>
2014-02-18 02:33:48 -0500

    Update source.rb

--- recipes/source.rb
+++ recipes/source.rb
@@ -10,7 +10,7 @@ directory "#{node[:ffmpeg][:source_prefix]}/ffmpeg-#{node[:ffmpeg][:version]}" d
     action :delete
 end
 
-remote_file "/etc/apt/source.list.d/faac.list" do
+template "/etc/apt/source.list.d/faac.list" do
     source "libfaac-dev.list.erb"
     notifies :run, resources(:execute => "apt-get update"), :immediately
 end
