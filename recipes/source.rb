46b4e70fe5c0e4f813f201eb8a682b04de775750
Pirate Dunbar <yarr@piratedunbar.com>
2014-02-18 08:38:36 -0500

    Update source.rb

--- recipes/source.rb
+++ recipes/source.rb
@@ -74,6 +74,6 @@ ark "ffmpeg" do
   url node[:ffmpeg][:source_url]
   prefix_root node[:ffmpeg][:source_prefix]
   version node[:ffmpeg][:version]
-  action [ :configure, :install_with_make ]
-  #autoconf_opts node[:ffmpeg][:autoconf_opts] if node[:ffmpeg][:autoconf_opts]
+  action [:configure, :install_with_make ]
+  autoconf_opts node[:ffmpeg][:autoconf_opts] if node[:ffmpeg][:autoconf_opts]
 end
