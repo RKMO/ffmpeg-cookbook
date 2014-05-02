e305779e06affac6c4bbcb2df9e514458f249d3f
Josh McDade <josh.ncsu@gmail.com>
2014-05-02 14:26:55 -0400

    compiles to /mnt/opt instead of /opt; do not install xmlto

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,8 @@ default[:ffmpeg][:install_method] = "source"
 
 default[:ffmpeg][:version] = "2.1"
 default[:ffmpeg][:source_url] = "http://ffmpeg.org/releases/ffmpeg-#{default[:ffmpeg][:version]}.tar.gz"
-default[:ffmpeg][:source_prefix] = "/opt"
-default[:ffmpeg][:options] = "/opt"
+default[:ffmpeg][:source_prefix] = "/mnt/opt"
+default[:ffmpeg][:options] = "/mnt/opt"
 default[:ffmpeg][:autoconf_opts] = %w[ --enable-shared --enable-postproc --enable-pthreads --enable-gpl
   --enable-version3 --enable-nonfree --enable-hardcoded-tables --enable-avresample --enable-vda --enable-libx264
   --extra-libs=-ldl --enable-libmp3lame --enable-libxvid --enable-libtheora --enable-libvorbis --enable-libass
@@ -11,6 +11,6 @@ default[:ffmpeg][:autoconf_opts] = %w[ --enable-shared --enable-postproc --enabl
 
 default[:ffmpeg][:packages] = %w[ libopencore-amrnb-dev libopencore-amrwb-dev libgsm1-dev libgpac-dev autoconf automake
   build-essential git libass-dev libgpac-dev libtheora-dev libtool libvorbis-dev texi2html zlib1g-dev clang libmp3lame0
-  libmp3lame-dev libavcodec-extra-53 libopenjpeg2 libopenjpeg-dev xmlto build-essential git-core checkinstall texi2html
+  libmp3lame-dev libavcodec-extra-53 libopenjpeg2 libopenjpeg-dev build-essential git-core checkinstall texi2html
   libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxfixes-dev zlib1g-dev automake autoconf
   libxvidcore-dev nasm libfaac-dev faac ]
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              "ffmpeg"
 maintainer        "Sergey Khaladzinski"
-version           "0.0.27"
+version           "0.0.28"
 
 depends "build-essential"
 depends "ark", "~> 0.3"
